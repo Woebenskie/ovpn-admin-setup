b0f1eb3d9460e7dd01b26ac78a65cdf8f9626edd
Woebenskie <42571709+Woebenskie@users.noreply.github.com>
2021-12-24 11:47:38 +0100

    Update ovpn-admin-run.sh

--- ovpn-admin-run.sh
+++ ovpn-admin-run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export PATH=$PATH:/etc/openvpn/easy-rsa
-/etc/openvpn/easy-rsa/ovpn-admin --ovpn.server=fal.vpn.svenvisser.tk:1194:udp --easyrsa.path="/etc/openvpn/easy-rsa/" --easyrsa.index-path="/etc/openvpn/easy-rsa/pki/index.txt"
+/etc/openvpn/easy-rsa/ovpn-admin --ovpn.server=$(hostname):1194:udp --easyrsa.path="/etc/openvpn/easy-rsa/" --easyrsa.index-path="/etc/openvpn/easy-rsa/pki/index.txt"
